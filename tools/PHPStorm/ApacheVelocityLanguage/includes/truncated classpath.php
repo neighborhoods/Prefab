955751beb9256f520e1aa84aed55f317e4083f01
Brad Wilson <brad@rhift.com>
2018-05-23 14:16:27 -0500

    i hate myself

---
+++ tools/PHPStorm/ApacheVelocityLanguage/includes/truncated classpath.php
@@ -0,0 +1,21 @@
+#set($isNeighborhoodsNamespaced = $NAMESPACE.startsWith("Neighborhoods"))
+#set($parts = $NAMESPACE.split("\\"))
+
+#set($part = "") 
+#set($truncatedClassPath = "")
+#set($awarePropertyName = "")
+#set($foreach = "")
+
+#foreach( $part in $parts )
+    #set($foreachCount = $foreach.count)
+    #if($isNeighborhoodsNamespaced)
+        #if($foreachCount != 1 && $foreachCount != 2)
+            #set($truncatedClassPath = $truncatedClassPath.concat($part))
+        #end
+    #else
+        #if($foreachCount != 1)
+            #set($truncatedClassPath = $truncatedClassPath.concat($part))
+        #end
+    #end
+  #set($awarePropertyName = $awarePropertyName.concat($part))
+#end
