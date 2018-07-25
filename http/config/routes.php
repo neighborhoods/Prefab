5fceaeb7e75385d71e0a5f2093670a0e7f09e61a
p-mucha <przemyslaw.mucha@55places.com>
2018-07-25 16:18:39 -0500

    Dummy routes

--- http/config/routes.php
+++ http/config/routes.php
@@ -1,13 +1,22 @@
 <?php
-
 declare(strict_types=1);
 
+use Neighborhoods\~~PROJECT NAME~~\MV1;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
 
 return function (Definition $applicationServiceDefinition, ContainerBuilder $containerBuilder): void {
-    // mv1/blip
-//    $handlerServiceDefinition = $containerBuilder->findDefinition('neighborhoods.~~PROJECT NAME~~.mv1.blip.handler');
-//    $applicationServiceDefinition->addMethodCall('get', ['/mv1/blip/{id:\d+}', $handlerServiceDefinition, 'blip']);
-//    $applicationServiceDefinition->addMethodCall('get', ['/mv1/blips', $handlerServiceDefinition, 'blips']);
+    // mv1/dummy
+    $mv1DummyHandlerServiceDefinition = $containerBuilder->findDefinition(MV1\Dummy\HandlerInterface::class);
+    $applicationServiceDefinition->addMethodCall(
+        'get',
+        ['/mv1/dummy/{id:\d+}', $mv1DummyHandlerServiceDefinition, 'dummy']
+    );
+
+    // mv1/dummies
+    $mv1DummyAbbreviatedHandlerServiceDefinition = $containerBuilder->findDefinition(MV1\Dummy\Abbreviated\HandlerInterface::class);
+    $applicationServiceDefinition->addMethodCall(
+        'get',
+        ['/mv1/dummies/{uris:}', $mv1DummyAbbreviatedHandlerServiceDefinition, 'dummies']
+    );
 };
