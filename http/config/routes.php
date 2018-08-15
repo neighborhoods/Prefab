07761cc3a6c9c55d536fd7cc4e92bddafc3c9786
Brad Wilson <brad@rhift.com>
2018-08-15 11:27:41 -0500

    Update route examples to include Repository dir path node

--- http/config/routes.php
+++ http/config/routes.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\Definition;
 return function (Definition $applicationServiceDefinition, ContainerBuilder $containerBuilder): void {
     // @team - delete me
     // mv1/DAO example
-//    $mv1DAOHandlerServiceDefinition = $containerBuilder->findDefinition(MV1\DAO\HandlerInterface::class);
+//    $mv1DAOHandlerServiceDefinition = $containerBuilder->findDefinition(MV1\DAO\Repository\HandlerInterface::class);
 //    $applicationServiceDefinition->addMethodCall(
 //        'get',
 //        ['/mv1/dao/{id:\d+}', $mv1DAOHandlerServiceDefinition, 'dao']
@@ -19,6 +19,10 @@ return function (Definition $applicationServiceDefinition, ContainerBuilder $con
     // mv1/DAOs example
 //    $applicationServiceDefinition->addMethodCall(
 //        'get',
-//        ['/mv1/daos/{searchCriteria:}', $mv1DAOHandlerServiceDefinition, MV1\DAO\HandlerInterface::ROUTE_NAME_DAOS]
+//        [
+//            '/mv1/daos/{searchCriteria:}',
+//            $mv1DAOHandlerServiceDefinition,
+//            MV1\DAO\Repository\HandlerInterface::ROUTE_NAME_DAOS
+//        ]
 //    );
 };
