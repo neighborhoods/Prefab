18b69521422f3a58a0e195a23539a75d6db1e265
jacobmalachowski <jacob.malachowski@55places.com>
2018-09-14 14:55:42 -0500

    add builder

--- src/HandlerInterface/Template.php
+++ src/HandlerInterface/Template.php
@@ -6,5 +6,5 @@ namespace Neighborhoods\Prefab\HandlerInterface;
 
 interface Template //extends RequestHandlerInterface
 {
-    public const ROUTE_NAME_TRUNACTEDDOAPLACEHOLDER = 'TRUNCATEDDAONAMEPLACEHOLDER';
+    public const ROUTE_NAME_TRUNCATEDDOANAMEPLACEHOLDER = 'TRUNCATEDDOANAMEPLACEHOLDER';
 }
