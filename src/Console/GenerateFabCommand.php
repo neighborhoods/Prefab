1b2bbb84f71fc9938c633bd69eba7a4761827e2a
jacobmalachowski <jacob.malachowski@55places.com>
2018-10-19 13:07:58 -0500

    doing the same as that http thing

--- src/Console/GenerateFabCommand.php
+++ src/Console/GenerateFabCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace Neighborhoods\Prefab\Console;
 
 use Neighborhoods\Prefab\Generator;
+use Neighborhoods\Prefab\Protean\Container\Builder;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -24,8 +25,6 @@ class GenerateFabCommand extends Command implements GenerateFabCommandInterface
 
     protected function execute(InputInterface $input, OutputInterface $output) : GenerateFabCommandInterface
     {
-        $this->getGenerator()->setProjectDir(__DIR__ . '/../../../../../')->generate();
 
-        return $this;
     }
 }
