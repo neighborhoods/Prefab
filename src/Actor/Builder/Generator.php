c1d640e53e050495df8246061aa4bbfffdc5b1d3
Timothy Rouke <timothy.rourke@55places.com>
2018-12-10 15:34:51 -0600

    MLSS-371 Fix code formatting

--- src/Actor/Builder/Generator.php
+++ src/Actor/Builder/Generator.php
@@ -22,6 +22,7 @@ class Generator implements GeneratorInterface
     protected const BUILDER_BUILD_METHOD_PLACEHOLDER = 'BUILDERBUILDMETHODPLACEHOLDER';
     protected const METHOD_PATTERN = "->set%s(\$this->record['%s'])\n";
     protected const METHOD_PATTERN_CONDITIONAL_SETTERS = <<<EOF
+
         if (array_key_exists('%s', \$this->record)) {
             $%s->set%s(\$this->record['%s']);
         }
@@ -84,7 +85,7 @@ EOF;
         }
 
         $methodString .= sprintf(
-            '\n        return $%s;',
+            "\n        return \$%s",
             $this->getMeta()->getDaoName()
         );
 
