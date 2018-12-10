d857ad78fc4888a0067e3b53506052e26c4d9919
Timothy Rouke <timothy.rourke@55places.com>
2018-12-10 15:33:05 -0600

    MLSS-371 Fix code formatting

--- src/Actor/Builder/Generator.php
+++ src/Actor/Builder/Generator.php
@@ -22,8 +22,6 @@ class Generator implements GeneratorInterface
     protected const BUILDER_BUILD_METHOD_PLACEHOLDER = 'BUILDERBUILDMETHODPLACEHOLDER';
     protected const METHOD_PATTERN = "->set%s(\$this->record['%s'])\n";
     protected const METHOD_PATTERN_CONDITIONAL_SETTERS = <<<EOF
-        ;
-
         if (array_key_exists('%s', \$this->record)) {
             $%s->set%s(\$this->record['%s']);
         }
@@ -85,11 +83,16 @@ EOF;
             }
         }
 
+        $methodString .= sprintf(
+            '\n        return $%s;',
+            $this->getMeta()->getDaoName()
+        );
+
         return str_replace(
             self::BUILDER_BUILD_METHOD_PLACEHOLDER,
             $methodString,
             $builtFile
-        ) . sprintf('\n        return $%s;', $this->getMeta()->getDaoName());
+        );
     }
 
     protected function getCamelCasePropertyName(string $property) : string
