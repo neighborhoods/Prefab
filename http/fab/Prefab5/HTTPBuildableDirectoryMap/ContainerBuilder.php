5cc62df2dda9dc11c456a07afb02ac4f3c8bc2d5
jacobmalachowski <jacob.malachowski@55places.com>
2019-05-01 13:42:12 -0500

    add fallback to old functionality

--- http/fab/Prefab5/HTTPBuildableDirectoryMap/ContainerBuilder.php
+++ http/fab/Prefab5/HTTPBuildableDirectoryMap/ContainerBuilder.php
@@ -24,13 +24,20 @@ class ContainerBuilder implements ContainerBuilderInterface
             'HTTP_' . str_replace('/', '_', $this->getDirectoryGroup())
         );
 
-        if (!isset($this->getBuildableDirectoryMap()[$this->getDirectoryGroup()])) {
+        $directoryGroupRoot = explode('/', $this->getDirectoryGroup())[0];
+
+        if (
+            !isset($this->getBuildableDirectoryMap()[$this->getDirectoryGroup()])
+            && !isset($this->getBuildableDirectoryMap()[$directoryGroupRoot()])
+        ) {
             throw (new InvalidDirectory\Exception)->setCode(InvalidDirectory\Exception::CODE_INVALID_DIRECTORY);
         }
 
         $this->getProteanContainerBuilder()->buildZendExpressive();
 
-        $routeBuildableDirectories = $this->getBuildableDirectoryMap()[$this->getDirectoryGroup()];
+        $routeBuildableDirectories =
+            $this->getBuildableDirectoryMap()[$this->getDirectoryGroup()] ??
+            $this->getBuildableDirectoryMap()[$directoryGroupRoot];
 
         $this->addBuildableDirectories($routeBuildableDirectories);
         $this->addWelcomeBaskets($routeBuildableDirectories);
@@ -38,8 +45,6 @@ class ContainerBuilder implements ContainerBuilderInterface
 
         return $this->getProteanContainerBuilder();
     }
-
-
 
     protected function addBuildableDirectories(array $httpBuildableDirectoryMap) : ContainerBuilderInterface
     {
