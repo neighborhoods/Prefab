d6a12a4f7b01bc944ca63f177f768ee9cd28c71d
jacobmalachowski <jacob.malachowski@55places.com>
2019-10-25 12:24:18 -0500

    Update actor to use correct static context builder and annotation processor

--- src/ActorConfiguration/Actor.php
+++ src/ActorConfiguration/Actor.php
@@ -14,8 +14,8 @@ interface Actor
     public const ANNOTATION_PROCESSORS = [
         [
             AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_KEY => self::ANNOTATION_PROCESSOR_KEY_DAO_PROPERTIES_AND_ACCESSORS,
-            AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\ActorInterface\DaoPropertiesAndAccessors\Builder::class,
-            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\DAOInterface::class,
+            AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\Actor\DaoPropertiesAndAccessors\Builder::class,
+            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\DAO::class,
         ],
     ];
 }
