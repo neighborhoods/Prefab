c16afaff5d5c58ae5c9c7f1b629059e5ebf14b3d
jacobmalachowski <jacob.malachowski@55places.com>
2019-10-25 14:24:03 -0500

    Add builder factory aware trait annotation processor

--- src/ActorConfiguration/Actor/Builder.php
+++ src/ActorConfiguration/Actor/Builder.php
@@ -11,6 +11,7 @@ interface Builder
     public const TEMPLATE_PATH = 'Actor/Builder.php';
     public const BUILD_METHOD_ANNOTATION_PROCESSOR_KEY = 'Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder-build';
     public const BUILD_FOR_INSERT_METHOD_ANNOTATION_PROCESSOR_KEY = 'Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder-buildForInsert';
+    public const BUILDER_FACTORY_AWARE_TRAITS_ANNOTATION_PROCESSOR_KEY = 'Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder-AwareTraits';
 
     public const ANNOTATION_PROCESSORS = [
         [
@@ -22,5 +23,10 @@ interface Builder
             AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_KEY => self::BUILD_FOR_INSERT_METHOD_ANNOTATION_PROCESSOR_KEY,
             AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\Actor\Builder\BuildForInsertMethod\Builder::class,
             AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\Actor\BuilderBuildForInsertMethod::class,
+        ],
+        [
+            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_KEY => self::BUILDER_FACTORY_AWARE_TRAITS_ANNOTATION_PROCESSOR_KEY,
+            AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\Actor\Builder\BuilderFactoryAwareTraits\Builder::class,
+            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\Actor\BuilderFactoryTrait::class,
         ],
     ];}
