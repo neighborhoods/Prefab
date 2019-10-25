0df0c96b74d82090b09a12f3d387bd33648fc904
jacobmalachowski <jacob.malachowski@55places.com>
2019-10-25 14:17:14 -0500

    Add build for insert method annotation processor

--- src/ActorConfiguration/Actor/Builder.php
+++ src/ActorConfiguration/Actor/Builder.php
@@ -10,11 +10,17 @@ interface Builder
     public const ACTOR_KEY = '<ActorName>/Builder.php';
     public const TEMPLATE_PATH = 'Actor/Builder.php';
     public const BUILD_METHOD_ANNOTATION_PROCESSOR_KEY = 'Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder-build';
+    public const BUILD_FOR_INSERT_METHOD_ANNOTATION_PROCESSOR_KEY = 'Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder-buildForInsert';
 
     public const ANNOTATION_PROCESSORS = [
         [
             AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_KEY => self::BUILD_METHOD_ANNOTATION_PROCESSOR_KEY,
             AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\Actor\Builder\BuildMethod\Builder::class,
             AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\Actor\Builder::class,
+        ],
+        [
+            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_KEY => self::BUILD_FOR_INSERT_METHOD_ANNOTATION_PROCESSOR_KEY,
+            AnnotationProcessorRecordInterface::KEY_STATIC_CONTEXT_RECORD_BUILDER => \Neighborhoods\Prefab\AnnotationProcessorRecord\StaticContextRecord\Actor\Builder\BuildForInsertMethod\Builder::class,
+            AnnotationProcessorRecordInterface::KEY_ANNOTATION_PROCESSOR_FULLY_QUALIFIED_CLASS_NAME => \Neighborhoods\Prefab\AnnotationProcessor\Actor\BuilderBuildForInsertMethod::class,
         ],
     ];}
