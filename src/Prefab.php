bc588309b455207b687b1a756fd7dd9e6da055db
Hrvoje Jukic <hrcajuka@gmail.com>
2021-01-20 07:55:40 +0100

    Replaced setProteanContainerBuilder() with setApplicationRootDirectoryPath() in Prefab class. Amend prefab script accordingly

--- src/Prefab.php
+++ src/Prefab.php
@@ -4,20 +4,21 @@
 namespace Neighborhoods\Prefab;
 
 
+use LogicException;
 use Neighborhoods\Prefab\Protean\Container\Builder;
 
 class Prefab implements PrefabInterface
 {
-
-    use Builder\AwareTrait;
-
+    protected $applicationRootDirectoryPath;
     protected $project_dir;
 
     public function generate() : PrefabInterface
     {
-        $this
-            ->getProteanContainerBuilder()
-            ->build()
+        $container = (new Builder())
+            ->setApplicationRootDirectoryPath($this->getApplicationRootDirectoryPath())
+            ->build();
+
+        $container
             ->get(GeneratorInterface::class)
             ->setProjectRoot($this->getProjectDir())
             ->generate();
@@ -28,7 +29,7 @@ class Prefab implements PrefabInterface
     protected function getProjectDir()
     {
         if ($this->project_dir === null) {
-            throw new \LogicException('Prefab project_dir has not been set.');
+            throw new LogicException('Prefab project_dir has not been set.');
         }
         return $this->project_dir;
     }
@@ -36,9 +37,26 @@ class Prefab implements PrefabInterface
     public function setProjectDir($project_dir) : PrefabInterface
     {
         if ($this->project_dir !== null) {
-            throw new \LogicException('Prefab project_dir is already set.');
+            throw new LogicException('Prefab project_dir is already set.');
         }
         $this->project_dir = $project_dir;
         return $this;
+    }
+
+    public function setApplicationRootDirectoryPath(string $applicationRootDirectoryPath): PrefabInterface
+    {
+        if (isset($this->applicationRootDirectoryPath)) {
+            throw new LogicException('Application Root Directory Path is already set.');
+        }
+        $this->applicationRootDirectoryPath = $applicationRootDirectoryPath;
+        return $this;
+    }
+
+    private function getApplicationRootDirectoryPath(): string
+    {
+        if (!isset($this->applicationRootDirectoryPath)) {
+            throw new LogicException('Application Root Directory Path has not been set.');
+        }
+        return $this->applicationRootDirectoryPath;
     }
 }
