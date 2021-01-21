ab77af5d9cc2a6edffdf0f72bc68135976875d6a
Hrvoje Jukic <hrcajuka@gmail.com>
2021-01-21 09:55:31 +0100

    Implemented initial builder for discoverable directories.

---
+++ http/fab/Prefab5/HTTPBuildableDirectoryMap/DiscoverableDirectories/Builder.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace ReplaceThisWithTheNameOfYourVendor\ReplaceThisWithTheNameOfYourProduct\Prefab5\HTTPBuildableDirectoryMap\DiscoverableDirectories;
+
+use LogicException;
+use ReplaceThisWithTheNameOfYourVendor\ReplaceThisWithTheNameOfYourProduct\Prefab5\HTTPBuildableDirectoryMap\DiscoverableDirectories;
+use ReplaceThisWithTheNameOfYourVendor\ReplaceThisWithTheNameOfYourProduct\Prefab5\HTTPBuildableDirectoryMap\DiscoverableDirectoriesInterface;
+
+class Builder implements BuilderInterface
+{
+    protected const YAML_KEY_BUILDABLE_DIRECTORIES = 'buildable_directories';
+    protected const YAML_KEY_WELCOME_BASKETS = 'welcome_baskets';
+    protected const YAML_KEY_APPENDED_PATHS = 'appended_paths';
+
+    protected $record;
+
+    public function build(): DiscoverableDirectoriesInterface
+    {
+        $discoverableDirectories = new DiscoverableDirectories();
+        $record = $this->getRecord();
+
+        if (isset($record[self::YAML_KEY_BUILDABLE_DIRECTORIES])) {
+            foreach ($record[self::YAML_KEY_BUILDABLE_DIRECTORIES] as $directory) {
+                $discoverableDirectories->addDirectoryPathFilter($directory);
+            }
+        }
+
+        if (isset($record[self::YAML_KEY_WELCOME_BASKETS])) {
+            foreach ($record[self::YAML_KEY_WELCOME_BASKETS] as $welcomeBasket) {
+                $discoverableDirectories->getWelcomeBaskets()->addWelcomeBasket($welcomeBasket);
+            }
+        }
+
+        if (isset($record[self::YAML_KEY_APPENDED_PATHS])) {
+            foreach ($record[self::YAML_KEY_APPENDED_PATHS] as $path) {
+                $discoverableDirectories->appendPath($path);
+            }
+        }
+
+        return $discoverableDirectories;
+    }
+
+    public function setRecord(array $record): BuilderInterface
+    {
+        if (isset($this->record)) {
+            throw new LogicException('Record is already set.');
+        }
+        $this->record = $record;
+        return $this;
+    }
+
+    private function getRecord(): array
+    {
+        if (!isset($this->record)) {
+            throw new LogicException('Record has not been set.');
+        }
+        return $this->record;
+    }
+}
---
+++ http/fab/Prefab5/HTTPBuildableDirectoryMap/DiscoverableDirectories/BuilderInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace ReplaceThisWithTheNameOfYourVendor\ReplaceThisWithTheNameOfYourProduct\Prefab5\HTTPBuildableDirectoryMap\DiscoverableDirectories;
+
+use ReplaceThisWithTheNameOfYourVendor\ReplaceThisWithTheNameOfYourProduct\Prefab5\HTTPBuildableDirectoryMap\DiscoverableDirectoriesInterface;
+
+interface BuilderInterface
+{
+    public function build(): DiscoverableDirectoriesInterface;
+
+    public function setRecord(array $record): BuilderInterface;
+}
